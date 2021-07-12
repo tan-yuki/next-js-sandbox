85ddf70c533ac2101aec1fc9bd3079ebf1351269
tan-yuki <a0673040@gmail.com>
2021-07-12 11:40:16 +0900

    Fix home style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import prisma from '../lib/prisma';
+import Head from 'next/head'
 import Link from 'next/link'
 
 export default function Home() {
   return <>
-    <Link href="/todo">TODO</Link>
+    <Head>
+      <title>Nextjs Sandbox</title>
+      <meta property="og:title" content="Nextjs Sandbox" key="title" />
+    </Head>
+    <h1>Menu</h1>
+    <ul>
+      <li>
+        <Link href="/todo">TODO</Link>
+      </li>
+    </ul>
   </>;
 }
